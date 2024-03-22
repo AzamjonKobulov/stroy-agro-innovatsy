b1f9a4c6137cc761e5fa80856193b64c7da07563
AzamjonKobulov <azamjonkobulov1998@gmail.com>
2024-03-22 12:13:42 +0500

    add/mob menu

--- public/script.js
+++ public/script.js
@@ -204,3 +204,26 @@ document.addEventListener('DOMContentLoaded', function () {
     successModal.classList.add('hidden');
   });
 });
+
+const mobileMenu = document.querySelector('#mobileMenu');
+const mobileMenuBtn = document.querySelector('#mobileMenuBtn');
+
+const toggleMobileMenu = () => {
+  mobileMenu.classList.toggle('-translate-y-full');
+  document.body.classList.toggle('overflow-hidden');
+
+  // const isOpen = !mobileMenu.classList.contains('-translate-y-full');
+  // document.querySelectorAll('.menuText, .hamburger').forEach((element) => {
+  //   element.classList.toggle('hidden', isOpen);
+  // });
+  // document.querySelectorAll('.closeMenuText, .close').forEach((element) => {
+  //   element.classList.toggle('hidden', !isOpen);
+  // });
+  // console.log('click');
+};
+
+mobileMenuBtn.addEventListener('click', toggleMobileMenu);
+
+document.querySelectorAll('#mobileMenu ul a').forEach((link) => {
+  link.addEventListener('click', toggleMobileMenu);
+});
